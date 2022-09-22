f65104c2ab31c6f8d91f450877312c1767a1ea7d
RocketGod <jeremy@umcontractors.com>
2022-09-22 13:46:18 -0700

    Add urh

--- Software/urh/src/urh/version.py
+++
@@ -1,5 +0,0 @@
-VERSION = "2.9.3" 
-
-if __name__ == '__main__':
-    # To read out version easy on command line for InnoSetup
-    print(VERSION)
--- Software/urh/tests/data/decode.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""
-Simple example external decoding
-Simply removes every second bit
-"""
-
-import sys
-bits = sys.argv[1]
-print("".join(b for b in bits[::2]))
